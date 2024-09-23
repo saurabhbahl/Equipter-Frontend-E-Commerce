8c8ccc89853d1ca90a8930196046efda81dff2f0
saurabhbahl <behl.saurabh@gmail.com>
2024-09-23 12:21:24 +0530

    Build Cash 4000 Page HTML

--- custom.js
+++ custom.js
@@ -26,4 +26,33 @@ jQuery(document).ready(function($){
           }
         ]
       });
+      $('.tab-slider').slick({
+        loop: true,
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+      });
+});
+jQuery(document).ready(function($) {
+  // Initialize all tab sections
+  $('.tabs-section').each(function() {
+    var $this = $(this);
+    
+    // Set the first tab as active and show the first tab content
+    $this.find('.tabs-nav li:first-child').addClass('active');
+    $this.find('.tab-content').hide();
+    $this.find('.tab-content:first').show();
+
+    // Handle tab clicks
+    $this.find('.tabs-nav li').click(function() {
+      // Only affect the current section
+      $this.find('.tabs-nav li').removeClass('active');
+      $(this).addClass('active');
+      $this.find('.tab-content').hide();
+
+      var activeTab = $(this).find('a').attr('href');
+      $this.find(activeTab).fadeIn();
+      return false;
+    });
+  });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,27 @@ module.exports = {
     extend: {
       colors:{
         'custom-orange': '#ea7600',
+        'custom-orange-100': '#F6841F',
         'custom-gray': '#444',
         'custom-gray-200': '#767676',
+        'custom-gray-300': '#f5f5f5',
+        'custom-gray-400': '#d9d9d9',
+        'custom-black-200': '#1e1e1e',
+        'custom-black-25': 'rgba(0, 0, 0, 0.25)',
       },
       fontFamily: {
         'work-sans': ['Work Sans', 'sans-serif'],
         'arial': ['Arial', 'sans-serif'],
         'robot': ['Roboto', 'sans-serif'],
+      },
+      fontSize: {
+        '64': ['64px', { lineHeight: '75px' }],
+        '32': ['32px', { lineHeight: '37px' }],
+        '27': ['27px', { lineHeight: '31px' }],
+        '25': ['25px', { lineHeight: '29px' }],
+        '19': ['19px', { lineHeight: '23px' }],
+        '17': ['17px', { lineHeight: '19px' }],
+        '15': ['15px', { lineHeight: '17px' }],
       },
       borderStyle: {
         inset: 'inset',
